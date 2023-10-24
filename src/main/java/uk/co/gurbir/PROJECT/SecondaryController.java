0d0b5a7f56c4755f3ab154859495043d4a747640
unknown <gurbir.singh.2021@live.rhul.ac.uk>
2023-10-24 16:03:09 +0100

    Project Setup

---
+++ src/main/java/uk/co/gurbir/PROJECT/SecondaryController.java
@@ -0,0 +1,12 @@
+package uk.co.gurbir.PROJECT;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
