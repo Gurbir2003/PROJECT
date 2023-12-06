ce7f0139e636b9a2f715725c0145f12abd4ab8d3
unknown <gurbir.singh.2021@live.rhul.ac.uk>
2023-12-06 23:18:42 +0000

    Added code to enable player movement and check if the movement is valid(by checking around it, if there is a empty space around it).

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -43,10 +43,22 @@ public class Board {
 	}
 	
 	public void displayPhysicalBoard() {
+		System.out.println("   0   1   2   3   4   5   6   7   8   9");
 		for(int row=0; row<NUM_ROWS; row++) {
 			for(int col=0; col<NUM_COLS; col++) {
+				if(col == 0) {
+					System.out.print(row + " ");
+				}
 				if(grid[row][col].getPiece() == null) {
-					System.out.print("??? ");
+					if (row == 4 && (col == 2 || col == 3 || col == 6 || col == 7)) {
+						System.out.print("### ");
+					}
+					else if (row == 5 && (col == 2 || col == 3 || col == 6 || col == 7)) {
+						System.out.print("### ");
+					}
+					else {
+						System.out.print("??? ");
+					}
 				}
 				else {
 					System.out.print(grid[row][col].getPiece().getPieceColor().getColor().charAt(0) + "" +
@@ -59,6 +71,76 @@ public class Board {
 	
 	public void placePiece(int i, int j, Piece p) {
 		grid[i][j].setPiece(p); 
+	}
+	
+	
+	public boolean hasFreeSquare(int row, int col) {
+		// Can't select Water square
+		if (grid[row][col].getType() == SquareType.WATER ||
+			grid[row][col].getPiece() == null) {
+			return false;
+		}
+		// Look up
+		if (grid[row-1][col].getType() != SquareType.WATER && 
+				grid[row-1][col].getPiece() == null) {
+			return true;
+		}
+		// Look down
+		if (grid[row+1][col].getType() != SquareType.WATER && 
+				grid[row+1][col].getPiece() == null) {
+			return true;
+		}
+		// Look left
+		if (grid[row][col-1].getType() != SquareType.WATER && 
+				grid[row][col-1].getPiece() == null) {
+			return true;
+		}
+		// Look right
+		if (grid[row][col+1].getType() != SquareType.WATER && 
+				grid[row][col+1].getPiece() == null) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean movePiece(int row, int col, String direction) {
+		// Look up
+		if (direction.equals("Up") && 
+				grid[row-1][col].getType() != SquareType.WATER && 
+				grid[row-1][col].getPiece() == null) {
+			Piece p = grid[row][col].getPiece();
+			grid[row][col].setPiece(null);
+			grid[row-1][col].setPiece(p);
+			return true;
+		}
+		// Look down
+		if (direction.equals("Down") && 
+				grid[row+1][col].getType() != SquareType.WATER && 
+				grid[row+1][col].getPiece() == null) {
+			Piece p = grid[row][col].getPiece();
+			grid[row][col].setPiece(null);
+			grid[row+1][col].setPiece(p);
+			return true;
+		}
+		// Look left
+		if (direction.equals("Left") && 
+				grid[row][col-1].getType() != SquareType.WATER && 
+				grid[row][col-1].getPiece() == null) {
+			Piece p = grid[row][col].getPiece();
+			grid[row][col].setPiece(null);
+			grid[row][col-1].setPiece(p);
+			return true;
+		}
+		// Look right
+		if (direction.equals("Right") && 
+				grid[row][col+1].getType() != SquareType.WATER && 
+				grid[row][col+1].getPiece() == null) {
+			Piece p = grid[row][col].getPiece();
+			grid[row][col].setPiece(null);
+			grid[row][col+1].setPiece(p);
+			return true;
+		}
+		return false;
 	}
 
 	/**
