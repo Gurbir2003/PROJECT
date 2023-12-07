a09d89abc9fa5dc63313a420a63751d035c4562a
unknown <gurbir.singh.2021@live.rhul.ac.uk>
2023-12-07 06:06:28 +0000

    Added JavaDoc for SquareType.java class

--- src/main/java/model/SquareType.java
+++ src/main/java/model/SquareType.java
@@ -1,5 +1,7 @@
 package model;
-
+/**
+ * The SquareType class is an enum type class and It is used to represent the type of square i.e. the square can either be made of GRASS(pieces can be placed and moved on grass) or WATER(pieces can't be placed on water)
+ */
 public enum SquareType {
 	GRASS, WATER;
 }
