80898869724dcffdeed707bc8bbbe865c57827b4
unknown <gurbir.singh.2021@live.rhul.ac.uk>
2024-03-31 10:29:47 +0100

    Added code which now allows user to place the pieces randomly along with adding a start and reset button.

--- src/main/java/uk/co/gurbir/PROJECT/StrategoController.java
+++ src/main/java/uk/co/gurbir/PROJECT/StrategoController.java
@@ -1,21 +1,28 @@
 package uk.co.gurbir.PROJECT;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
-import javafx.scene.input.DragEvent;
+import javafx.scene.layout.Pane;
 import model.Timer;
 
 public class StrategoController {
+	
+	private static final int CELL_WIDTH = 80;
+	private static final int CELL_HEIGHT = 80;
+	
+	private boolean gameStarted = false;
 
     @FXML
     private Label timer_label;
@@ -60,11 +67,6 @@ public class StrategoController {
     @FXML private ImageView BB_5_image_view;
     @FXML private ImageView BB_6_image_view;
     @FXML private ImageView BF_image_view;
-
-
-
-
-
     
     private ImageView[] player_images;
     
@@ -78,46 +80,37 @@ public class StrategoController {
             this.row = row;
             this.column = column;
         }
+        
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Position position = (Position) o;
+            return row == position.row && column == position.column;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, column);
+        }
     }
     
     private ImageView draggedImageView = null;
     
     @FXML private GridPane map_grid_pane;
+    
+    @FXML private Button placement_button;
+    @FXML private Button start_button;
+    @FXML private Button reset_button;
 
     @FXML
     public void initialize() {
-        Timer timerRunnable = new Timer(0, 0, 0) {
-            @Override
-            public void run() {
-                System.out.println("Thread started...");
-                try {
-                    while (!Thread.currentThread().isInterrupted()) {
-                        setSeconds(getSeconds() + 1);
-                        if (getSeconds() == 60) {
-                            setSeconds(0);
-                            setMinutes(getMinutes() + 1);
-                            if (getMinutes() == 60) {
-                                setMinutes(0);
-                                setHours(getHours() + 1);
-                            }
-                        }
-                        String time = this.toString();
-                        Platform.runLater(() -> timer_label.setText(time));
-                        Thread.sleep(1000);
-                    }
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt(); // Restore the interrupted status
-                    System.out.println("Timer thread interrupted");
-                }
-            }
-        };
-        Thread timerThread = new Thread(timerRunnable);
-        timerThread.start(); // Start the timer thread
+    	start_button.setText("Start");
+    	start_button.setDisable(true);
         
         setupPlayerImageArray();
         
         for(ImageView image: player_images) {
-        	System.out.println(image.getId());
         	setupDraggablePiece(image);
         }
         
@@ -132,35 +125,71 @@ public class StrategoController {
             Dragboard db = event.getDragboard();
             boolean success = false;
             if (db.hasImage() && draggedImageView != null) {
-                double cellWidth = 80;
-                double cellHeight = 80;
+                double cellWidth = CELL_WIDTH;
+                double cellHeight = CELL_HEIGHT;
                 int newColumn = (int) event.getX() / (int) cellWidth;
                 int newRow = (int) event.getY() / (int) cellHeight;
 
-                map_grid_pane.getChildren().remove(draggedImageView);
-                
-                draggedImageView.setFitWidth(cellWidth);
-                draggedImageView.setFitHeight(cellHeight);
-
-                map_grid_pane.add(draggedImageView, newColumn, newRow);
-                imagePositions.put(draggedImageView, new Position(newRow, newColumn));
-                
-                success = true;
+                if (newRow >= map_grid_pane.getRowConstraints().size() - 4) {
+                    Position newPosition = new Position(newRow, newColumn);
+                    
+                    if (!isPositionOccupied(newPosition) || isSwappingWithItself(newPosition)) {
+                        moveImageViewToNewPosition(draggedImageView, newPosition);
+                        success = true;
+                    }
+                    else {
+	                    if (isPositionOccupied(newPosition)) {
+	                        ImageView pieceAtNewPosition = getPieceAtPosition(newPosition);
+	                        Position oldPosition = imagePositions.get(draggedImageView);
+	
+	                        if (oldPosition != null) {
+	                            map_grid_pane.getChildren().removeAll(draggedImageView, pieceAtNewPosition);
+	
+	                            map_grid_pane.add(draggedImageView, newPosition.column, newPosition.row);
+	                            map_grid_pane.add(pieceAtNewPosition, oldPosition.column, oldPosition.row);
+	
+	                            imagePositions.put(draggedImageView, newPosition);
+	                            imagePositions.put(pieceAtNewPosition, oldPosition);
+	                            success = true;
+	                        }
+	                    } else {
+	                        map_grid_pane.getChildren().remove(draggedImageView);
+	
+	                        draggedImageView.setFitWidth(cellWidth);
+	                        draggedImageView.setFitHeight(cellHeight);
+	
+	                        map_grid_pane.add(draggedImageView, newColumn, newRow);
+	                        imagePositions.put(draggedImageView, newPosition);
+	
+	                        success = true;
+	                    }
+	
+	                    if (success && areAllPiecesPlaced()) {
+	                    	start_button.setDisable(false);
+	                        System.out.println("All pieces have been placed. Game can start.");
+	                    }
+                    }
+                }
             }
+
             event.setDropCompleted(success);
             event.consume();
         });
+        
+        setupWaterCells();
     }
     
     
     private void setupDraggablePiece(ImageView imageView) {
         imageView.setOnDragDetected(event -> {
-            draggedImageView = imageView;
-            Dragboard db = imageView.startDragAndDrop(TransferMode.MOVE);
-            ClipboardContent content = new ClipboardContent();
-            content.putImage(imageView.getImage());
-            db.setContent(content);
-            event.consume();
+        	if (!gameStarted) {
+	            draggedImageView = imageView;
+	            Dragboard db = imageView.startDragAndDrop(TransferMode.MOVE);
+	            ClipboardContent content = new ClipboardContent();
+	            content.putImage(imageView.getImage());
+	            db.setContent(content);
+	            event.consume();
+        	}
         });
 
         imageView.setOnDragDone(event -> {
@@ -169,7 +198,6 @@ public class StrategoController {
             }
             event.consume();
         });
-        System.out.println("Piece id: " + imageView.getId());
     }
     
     private void setupPlayerImageArray() {
@@ -214,5 +242,124 @@ public class StrategoController {
         player_images[37] = BB_5_image_view;
         player_images[38] = BB_6_image_view;
         player_images[39] = BF_image_view;
+    }
+
+    private boolean areAllPiecesPlaced() {
+        return imagePositions.size() == 40;
+    }
+    
+    private boolean isPositionOccupied(Position position) {
+        return imagePositions.containsValue(position);
+    }
+
+    private ImageView getPieceAtPosition(Position position) {
+        for (Map.Entry<ImageView, Position> entry : imagePositions.entrySet()) {
+            if (entry.getValue().equals(position)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+    
+    
+    @FXML
+    void randomPlacement(ActionEvent event) {
+        int minRow = map_grid_pane.getRowConstraints().size() - 4;
+        int maxRow = map_grid_pane.getRowConstraints().size() - 1;
+        int columns = map_grid_pane.getColumnConstraints().size();
+
+        for (ImageView piece : player_images) {
+            Position randomPosition;
+            do {
+                int randomRow = minRow + (int) (Math.random() * ((maxRow - minRow) + 1));
+                int randomColumn = (int) (Math.random() * columns);
+                randomPosition = new Position(randomRow, randomColumn);
+            } while (isPositionOccupied(randomPosition));
+
+            map_grid_pane.getChildren().remove(piece);
+            
+            piece.setFitWidth(CELL_WIDTH);
+            piece.setFitHeight(CELL_HEIGHT);
+            
+            map_grid_pane.add(piece, randomPosition.column, randomPosition.row);
+            imagePositions.put(piece, randomPosition);
+        }
+
+        if (areAllPiecesPlaced()) {
+            System.out.println("All pieces have been randomly placed. Game can start.");
+            placement_button.setDisable(true);
+            start_button.setDisable(false);
+        }
+    }
+    
+    private boolean isSwappingWithItself(Position newPosition) {
+        Position currentPosition = imagePositions.get(draggedImageView);
+        return currentPosition != null && currentPosition.equals(newPosition);
+    }
+
+    private void moveImageViewToNewPosition(ImageView imageView, Position newPosition) {
+        if (map_grid_pane.getChildren().contains(imageView)) {
+            map_grid_pane.getChildren().remove(imageView);
+        }
+        GridPane.setColumnIndex(imageView, newPosition.column);
+        GridPane.setRowIndex(imageView, newPosition.row);
+        map_grid_pane.getChildren().add(imageView);
+        imagePositions.put(imageView, newPosition);
+    }
+    
+    private void setupWaterCells() {
+        int[] waterRows = {4, 5}; // Rows for water cells
+        int[] waterColumns = {2, 3, 6, 7}; // Columns for water cells
+
+        for (int row : waterRows) {
+            for (int col : waterColumns) {
+                Pane waterPane = new Pane();
+                waterPane.setStyle("-fx-background-color: #0000ff;");
+                map_grid_pane.add(waterPane, col, row);
+            }
+        }
+    }
+    
+    @FXML
+    void startGame(ActionEvent event) {
+    	gameStarted = true;
+        startTimer();
+        start_button.setDisable(true);
+        System.out.println("Game started!");
+    }
+    
+    @FXML
+    void resetGame(ActionEvent event) {
+    	System.out.println("Reset done");
+    }
+    
+    private void startTimer() {
+    	Timer timerRunnable = new Timer(0, 0, 0) {
+            @Override
+            public void run() {
+                System.out.println("Thread started...");
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        setSeconds(getSeconds() + 1);
+                        if (getSeconds() == 60) {
+                            setSeconds(0);
+                            setMinutes(getMinutes() + 1);
+                            if (getMinutes() == 60) {
+                                setMinutes(0);
+                                setHours(getHours() + 1);
+                            }
+                        }
+                        String time = this.toString();
+                        Platform.runLater(() -> timer_label.setText(time));
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    System.out.println("Timer thread interrupted");
+                }
+            }
+        };
+        Thread timerThread = new Thread(timerRunnable);
+        timerThread.start(); // Start the timer thread
     }
 }
